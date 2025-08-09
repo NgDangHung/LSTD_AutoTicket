5d3e61a3df302d6fd55bb91e65eba5d4544c7ee3
NgDangHung <hn1862002@gmail.com>
2025-08-09 13:18:14 +0700

    Change cert key because of using different domain

--- public/sign-message.js
+++ public/sign-message.js
@@ -43,33 +43,33 @@
  */
 var privateKey = 
 "-----BEGIN PRIVATE KEY-----\n" +
-"MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDFOKq6WqdxDb5O\n" +
-"S1YDeNl/zW5zL0aTO+/0GYpGwpGOKfQwyEeY3zl8zf8izcp9MmBlluAZo8w/U9VS\n" +
-"GoTEs68rl1G5wJ5LHI7b6K6UfqG1GVLPUFLIsxrIw0xlU80yMVbQXJsqOmyi8ESf\n" +
-"xaETgEgi3bCRKMqdXOs4kPd6NZtQMQADhBYXJkDEoz0kuIP6FydWlaJNkK25fVox\n" +
-"Vx0QzL3NOEhNgbKoTwTgQU6X9adeRXoFS/5RmSSAk6jd5JVKn9wSKm12Qy3+W+cS\n" +
-"9SwVR6cBTTTVAXF6fcyb9z9U34FtWw2MjMeCP3dCKYw8n7ERrYUooXVDnh3CX8t9\n" +
-"nRmvCiKNAgMBAAECggEAAb1INDCMfYDDhuk/LvSn5mqnaLioBLBoo1x+TbV1ly1j\n" +
-"i/KYPw7o8OLYNzRdbtE87GhyPYemS4CvxvOY7Csxhk7lV6ANRVhRIjthHsonHvPE\n" +
-"OTPaJw79U3v1dodQrfqla10FuU+emalI0afqjcN532TZI86aSt0A7hTJOfizD/f6\n" +
-"suBeS/KKtSrwYmqz5aazSiZuYQrXWicO6PPPkq7Ze4FRyUkaXmeQaF7OYW81J2jH\n" +
-"1jZ9RoKakTIeF1Msw394QGrhb2xPd6XBgPoetGiTpe0G9SDBYmA3GXn7DoysH2Nu\n" +
-"XtnuXrSZwvt4lmr+aVbQWVXCOtLDKAqgD0OfszLQwQKBgQDoEXEktcmXGxpv/7fS\n" +
-"ccFBobXZitu1sW6i0QhDbTbzxeSmEkRqyfhtMFPNgTu3mdAdI+vgHP/IRaO7rUBE\n" +
-"C8yfqDzoJ/XnAtIPKahPgFBdPvZ6SoyftZZTmiqostbWhEsWTGuWM+YEaBdtDXwN\n" +
-"sEXj4QzfM6RQ27RZ/4XAjFqseQKBgQDZj0avsLa5vEr+Jkr2eCBU8TuspdIGpm+v\n" +
-"ZjB/9AHI/Fd9pSShjcJNTtYj+MqBp0FvCt0+ibDM0TjhCRr6I0L1mUKfvumr++Hq\n" +
-"MFT5lGX9W0kM2k6AqLxM2lhPiHcYuW0VPjl0WIyecP+10lByg2inahRWBq2hI+M0\n" +
-"a6EbpnF5tQKBgApfJTBf04emVXMl+aA1J074nDkBxLoFGQ27y+O/kNIqSQLHFDm1\n" +
-"lx3CKCyqFvIotK1oupUElsXrntq3boTcBiQoj7hMeOf/L0YwNW6ALJdORhPHGyvU\n" +
-"IMjRvhBWFdX2iaiQd8p5vVD7GfjsvluaQjViDII5O6HFf/T5v4+Qdkj5AoGAGS30\n" +
-"72aTb2G0iD+kU2xJyar1ziCO80gWCwXXvSdfgYR7F8aiO7RtdYcXT6h+v4y9DP4D\n" +
-"5zYGaV4ZqCnmnmawXD51sIlnAAmx9unLG+up9i23ga5OgaOFz5tBt0rDBfCICHpg\n" +
-"KfAJrljDr6Z8uxQsa5viaKZzOAAwTqPaTJnx7jUCgYBVRvVH/os7d9JzeAgyyyKd\n" +
-"WgDBzDmk5NrEizklWm+dPomVZU2n1MTO4P6ePQ7Ihpa6n6LQZ4K08UCvd49dgkVE\n" +
-"1pd7a34VVl1pG/f1l1rz3RRRoL4VYTlAKza2rAst0o3Ejb5NRbBAQfhN7ewdUgHv\n" +
-"vBhkReHLxIfbXDLob6BUcg==\n" +
-"-----END PRIVATE KEY-----"
+"MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQClY4u6jQoErfat\n" +
+"4rzMauFMzEVVZCXyp9kWnqvSYLjKV2UsGvxxtUxlDZn0ZsVeQCVKaLIFXLQyHTcS\n" +
+"xgrThwLlU5Z2gz7a1BQTtQ0TQXl4KX7rpihuQ4yWPt7suuQQ5fpPPZBJscIQYvwd\n" +
+"JCeg2QEju3NIY5YSJo8EWtzz47oK/jfucyMCppOCfNXcyPgVVKWZhw1LhfL/xq+8\n" +
+"303dxsFqh/sqytXpl6zuWA45nFjmYshWaGYhH53Q3lIyPU7CowGqD2FODymkJnOo\n" +
+"xzX0dFo2lFgmCXhw+CMQWYvSCOEU3+H4RV0+nP/BOqtsvv7sRzrLQW3/mNoycHLY\n" +
+"/8sc/OZ3AgMBAAECggEACSp29DCTRVj6hhD06Bp3bi7N93TCWn52d8DQcD22dJ+/\n" +
+"dnKm/xzXd2McOqo2kdwi8F0dvuQwKBqQ0paaw2zE3xoMUp2JnCk451b733P3acV9\n" +
+"0ZXyU4hb1wr0PWyTkix2/F8eJqZD/Ek4FrdcT7v5XNY9vQIWL0znVTCrGSjd/b11\n" +
+"MtkW6PKOZbyFfRz7WFBkdQpaAZNQcOYBJelWO6wFLy73HiIjQ4VXr1YEEHQ+uQHq\n" +
+"uXwfgbaPHtdk5zVKB5qfwNgjhvwU6dh/vCoZ92q54VHpsunFsXCjlYp4t6gM/1bU\n" +
+"dRBmeESeWzvC+ABP4BTBffqSF+lfZfMgRGLqfCkAtQKBgQDbxUNOQKaHwZ2mXXIr\n" +
+"LHy2fozmQ2+x90j3hjcOo7FUvhtP8DDEeoTBoymm5UNzLfDICx6PWzEU2J0QFjHu\n" +
+"nePuwCGSdF9dPZox2loEKO6KXWDzECSYsJiZIjbYUKJBY5q/47Fz2KRKqnvuJA0I\n" +
+"J3qt97IQZzdt3vhrR1ApJWnnkwKBgQDAp0Viw4SYb/fF+X5Zd6k0ZSTcrANnVfsr\n" +
+"3gP9pY1+IQIcvsZqEL1g148/4ZLjNKB6gPeTWjlKBnOCuGcjUvZjWPAAhbAwMJQ7\n" +
+"J4DviNO1y+8uT+yDkx6lsWUvEtlmXf8Kt7dVCNETtTaTp7odCyrQ87dQfdxzKlXO\n" +
+"BMhD+4HMDQKBgASPg2YMUWhfzDW3mrbqpWvkFGhy3c6Doy//yQS0wOlRSJ/QDDMH\n" +
+"l9ms2SBtgYjSWzzLfkbbTZlxJVFAJKke/avmsMupPpFgxeJi9ZtAfjLA3VIt85R3\n" +
+"lbMX0quKgNN6jauNvhD5Xe9uukOm295fc1xI15kfizbseZtAT5mK/TehAoGAaJXm\n" +
+"KxmtT/vo7Mbm+rblWpZWDp4rMtwQgN4EGJzZVVGiDezbVhVEWeBr2eghj8qwHBEL\n" +
+"Viky1Kbv6lUlWJ3fOEu97uqknWSh9AMcEe2b0SOm8uNcqIkjqBvHrGh1MQdyP2VA\n" +
+"157zsdk8iagSd1XqpojISaAqHzMqZa9e8NkGu7kCgYAOp6rmnxOD3MFwSnueg3v3\n" +
+"JYxh5Fa6HvvhomPEoGN7kv2ntVyCyoydh+7R1F+NdZHxkyz9Tfh17jnQ33FTmBcK\n" +
+"ub3Z7AoJF/r6KxMa5oo69nnUX3HOCqbyK+1USPTMuSrEjcYsHCDKjdvTmwZMp99Q\n" +
+"wIkEOvzIHJIddG3N1ZmLtA==\n" +
+"-----END PRIVATE KEY-----\n" 
 
 qz.security.setSignatureAlgorithm("SHA512"); // Since 2.1
 qz.security.setSignaturePromise(function(toSign) {
