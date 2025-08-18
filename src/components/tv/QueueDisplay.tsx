fcd86231354fc00f07345f494a44a9c3290b4125
NgDangHung <hn1862002@gmail.com>
2025-08-19 00:22:58 +0700

    Sync UI config.header beside time component

--- src/components/tv/QueueDisplay.tsx
+++ src/components/tv/QueueDisplay.tsx
@@ -772,7 +772,7 @@ export default function QueueDisplay() {
               TRUNG TÂM PHỤC VỤ HÀNH CHÍNH CÔNG
             </h1>
             <h1 className="text-5xl font-bold text-red-700" style={{ lineHeight: '1.3' }}>
-              {config.header}
+              {config.header.toUpperCase()}
             </h1>
             <p className='text-2xl font-extrabold text-red-700 mt-3' style={{fontSize: '2rem'}}>
               Hành chính phục vụ 
@@ -784,7 +784,7 @@ export default function QueueDisplay() {
         <div className="flex justify-between items-center" style={{flexDirection: 'row-reverse'}}>
           <h2 className="text-2xl text-red-700 font-bold italic" style={{position: 'relative',top: '-50px',left: '-180px', fontSize: '2rem'}}>
              {/* <span>{new Date().toLocaleTimeString('vi-VN')}</span> - Phường Tân Phong,  Ngày {new Date().toLocaleDateString('vi-VN')} */}
-             Phường Tân Phong,  Ngày {new Date().toLocaleDateString('vi-VN')}
+            {config.header},  Ngày {new Date().toLocaleDateString('vi-VN')}
           </h2>
         </div>
       </>
