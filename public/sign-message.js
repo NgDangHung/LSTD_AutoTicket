3b3061e9961bde0d163ba3990170c832b61dc0fa
NgDangHung <hn1862002@gmail.com>
2025-08-27 08:19:11 +0700

    Replace wrong key after merging from staging-local

--- public/sign-message.js
+++ public/sign-message.js
@@ -41,34 +41,33 @@
  *     5. IMPORTANT: Before deploying to production, copy "jsrsasign-all-min.js"
  *        to the web server.  Don't trust the CDN above to be available.
  */
-var privateKey = "-----BEGIN PRIVATE KEY-----\n" +
-"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcOaMBVIp5iQDT\n" +
-"Ni7A580OT5oMkDXeu5xz+XsZsW4++ToC7vOuKVoIhLW9001fX5EQjgx2GCsExRoR\n" +
-"xYgPt2CWIpLyGoWquUb21kGr/mYF5KqqT6E9UBWclC85MrEjkAWZKHtZKt1RXwvq\n" +
-"LYd33W6/9FWnLnz6bmRxD8Zj+Z4y5JzGbfjftFWBOFzPRX+hwEbDyIHhFpQ0RSxR\n" +
-"MfvjGj6SV1lPXMEja0f4xQ3aYsH+8O/GrSbwQ3eGGLM37ALjaqNY0NJjfKGudXdB\n" +
-"dJLmPX/CrVaef5uc7hbT9Cvaofr76cMiFW+tu2x+nQZHXaFuXH0R7+Ue4cruBP7t\n" +
-"F1J3UBo5AgMBAAECggEAFd3pufY2ZTy1rqMyx4AEwALmq5BurunvTiMzplCUPy/B\n" +
-"Qz0NiGmt+UpPHvUCGeIb/foy0c+Sxu+Yx/K2kMPvNN0kPOKmCtaIjbYm0XCKWhOz\n" +
-"YAOV4agH61MZ8MqgJOU2LxfiTo9naB7hSJyCduQPbQMydY4TxgyLz9+mffMRUqFq\n" +
-"Cn3vPtaTpm5fIl8P29G6payIBF0qdT2hTT7inmkfy9IlWoLysJXLYuAS9wGHdK6B\n" +
-"Bg6ru9IB1ekXC81RnXueRsLQOm9tHwQ92q/6YnNFBnYrsH0C0fI1uoGlb2ygQ2rj\n" +
-"IdNdcOVYBscqj95vJbd+hUjw8q61kY0zqsV2InL6QQKBgQD/PdYlAba/DdB4qxT2\n" +
-"ZcdJaxnDSLSnebJBTXgVF5ihRa5YN8wWgmc15i8FSXVPzkGoaDtUb5EGrtFa0adc\n" +
-"1CII8men/5II4ZIZkZR2fuQwPk6XYjcZodUpNPmgDEHfdMIE7KhI173CRZQXAACj\n" +
-"fn8ZwS2kZCpMnWesuWVPFBSVQQKBgQDc4Sm/+tp7bcjF7HJr5rHhB8XdLmh93hRm\n" +
-"I4o6EOg/ziPheKTxep2j53YPrvqErTUG0cbg7RQ73PEQihevs2znWpIjezRaKdvc\n" +
-"reP0H1S6fPYpIKYa5uZopzhuIZY/KI5sXkVusyTlFdo6y56XOWP8OpPVFJo5dGsE\n" +
-"LpYAf+lu+QKBgQCekcEXbpAk86UfQlIcnbCIeSQhQWsSXLWUgldm6yjkkFC297sw\n" +
-"BMWwI5f0teVOtjnuMQsi8sjrmPHxdSx/rD6UxlwFb+4+3DyS6/GLCFwlaKAUxmyq\n" +
-"3PJ7zjCC8Pp/o57hxlDqKDX4mpddDqBW8kBuNKhxZ9UrkeKV2PwKe9QpwQKBgD7P\n" +
-"9Tx4DNue28KVO/C9WQZiXH1KrkJk/i4Rm0Rp/HHwoDmFQnq9YMj0kJljLDDAxNyc\n" +
-"mU7rfJ6NdSw76QJ5JtYf5oEaj0e4saJu/O02X7Tsxl/pvfLWiN1n+F8xQ/XQBbiB\n" +
-"koMphKt07Gtd7kqY8nsO+W1V/4mpNqmoydV4ipyZAoGAVgKaCATAQkrK/f9fRBu1\n" +
-"jlJy0RDI5qkmAxclN/6Fn/H7B7Ws9jp9WYh8kkQzoGCBi8V6qVBNy/3URpu8NErI\n" +
-"6PRmBBPQMbKYpzfiM6H17e1zCKY61ak4f0t3vCQZ9i8Yng6BFr/rWqQ0QpxWy4/S\n" +
-"cSMEXiq8dD5uwyHq926Di30=\n" +
-"-----END PRIVATE KEY-----\n" 
+var privateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDFJrz71Nj2KVmR\n" +
+"8LBj7ovgC049m8Bya5AerOA/98zizcVTnGQKZczw8ccTzELupxjEmxaM/jTPVgDE\n" +
+"zLhjJFSIYzEgDXLyQD/8SABAjbGI2wRajYyD9TnT8UBOVmLepEV7d8LRh6IfUiQa\n" +
+"vFE6VpXI1+slN5TjCOOoK62/KY+ReLmngIRncuvD3NRwBhjtwDv+hknY+TtPNxM2\n" +
+"/cSDhfwBNZaf0UQKnpvK/xRL4OoKYUH/6Wjxt24+2isCGBxWS5Thi6dayPT36jFq\n" +
+"q4gPmY4GNO00g0A4hZMSWA8sK+3q0o72/pg8BuJoL4vYMW9q0eYJW1eyNtxAj9Gj\n" +
+"WLKqs9W/AgMBAAECggEATH5fTQakt74LdsiAaAWiV+eLD/RikwKRBJXm3Gr1H/re\n" +
+"2TYQysf84G+u4kuanKZQwde6xDHNo7Smz7TuxejOs6xo3xrYwo2TRftZvbI8c/Em\n" +
+"m0vYfUqkiUtNvBdgfSoXm3Q4LrwFd0mToJv4mD4IF8lO/z21kvFcA2APJeCmR7Z0\n" +
+"1qyA+S/2PvEPokkTFzeQpRJQvoXwOQb21sV+B/xanJBhu6ldkFaGTH4QVJDIaerw\n" +
+"kmi9Oz7svfIAYgO080jOdbr1E1ZS7EvpoksVY28lIif7TCs3JslWbH9zR6H93dbD\n" +
+"k5doSQ8WEyujKo0Dlahdw8lrrd1IEFWpsWjSWHjMwQKBgQDywf9388RL8S/2qiQJ\n" +
+"tSVllBU+LKx5m0iBMQpdqBZRyxn0gCSfxPTxg5DSho3tUWbaFC/YpoD4tErJfipm\n" +
+"m/7PauuEHZ8sNYXHSlNdKf61o1gFHxasFY/NWQePv00NY2Bjj4Qe2OVT+ovBvP1b\n" +
+"LHNjOOhNczCwAXU1m2E8QALH6QKBgQDP593V6JQC2/wXxOjPoYXwQ1ltw4JJ8te9\n" +
+"A8XZAmTi8X89U3iuKVoGEMemOCokmn/1VmNts5esI65yRIqx6YUDAvRGXzz4dB40\n" +
+"Ge8lJzNDfie287uOW5NWwo7MboyW1e1NfVsGKx783iN2w7GUuFlnBqlpIX8BYeFd\n" +
+"VtTVFHDPZwKBgQCkranKC5l8Eu+Am0a7NyFZWS1X8iJGlGqJCWWIxz/s2KSQ0mUF\n" +
+"5Sxssd2KkzCwbuogwlT62WjgTh31thq5ZNVki5mdFCMocF6CJQ+MQbLCMT7CqqZM\n" +
+"sovMt7qDbzir/mAi6fasnmJjp7ErCjOsp+jWxIICGERNAZArf25k2t+IEQKBgF0u\n" +
+"YcmmtPfXGtSYZ3Pvw/Ucam98qd903OaVU05g+VCRxmJ5D+SXGgcP7Dt+frzrFiCi\n" +
+"eGvJi1gWofAK8lKtA/WGlG8Z7ly4xnS5juqK+Dnux+QCt3QlbRL8nj3X6mEMPpB1\n" +
+"d597IidZdHGdYfDy8vbvEGzTG3ZGJVjNOxTJVYpPAoGAT1xEmvAetbT9TL42653/\n" +
+"7VMdheQc7HwMBS1Wq2XKpUHxmBzl0Rlgmvw7Yrb/lgowV04a8fUICmvJ5zOW/yiK\n" +
+"ICWCIDqwo/rjUsblnwmpbRPnu9FxwZjpwEecYFm6kjH/VLWOhigjepj0UTpA5ARt\n" +
+"kYm5lGCilf7Y4CRci2qtCpc=\n" +
+"-----END PRIVATE KEY-----";
 
 
 qz.security.setSignatureAlgorithm("SHA512"); // Since 2.1
